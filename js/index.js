4f400f50911981da3f1ad6b31bc56eb0290f45d0
brandongarrett <72109485+brandongarrett@users.noreply.github.com>
2020-11-04 14:58:29 -0600

    trying

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+// window.onload = () => {
+//   document.getElementById('start-button').onclick = () => {
+//     startGame();
+//   };
 
-  function startGame() {}
-};
+//   function startGame() {}
+// };
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = 'red';
+
+let img = new Image()
+img.src = 'road.png'
+img.onload = function () {
+  ctx.drawImage(img, 500, 500, 50, 50)
+}
+
+let car = {
+  x: 0,
+  y: 0,
+  h: 100,
+  w: 100
+}
